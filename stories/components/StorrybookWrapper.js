92d0ea744ff906de2d00554b8a422da391400911
Sverre Stensby <sstensby@no.imshealth.com>
2022-09-13 15:47:12 +0200

    D: Compatability - Removed style loaders (messing with running and installing on some systems).

--- stories/components/StorrybookWrapper.js
+++ stories/components/StorrybookWrapper.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import './StorybookWrapper.scss'; //eslint-disable-line
-
 export default function StorybookWrapper({children}) {
   return (
     <div className="storybook-wrapper">{children}</div>
